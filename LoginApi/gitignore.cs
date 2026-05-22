a278f493b14603b4f8809ed9fa3d313b0c0bde93
mgrsantos <gabrielasaantos294@gmail.com>
2026-05-22 16:03:19 -0300

    fix: gitignore corrigido

---
+++ LoginApi/gitignore.cs
@@ -0,0 +1,25 @@
+﻿# Build
+using LoginApi.Models;
+
+bin /
+obj /
+
+# Visual Studio
+.vs /
+*.user
+*.suo
+
+# Banco de dados
+*.db
+*.db - shm
+*.db - wal
+
+# Dados locais
+users.json
+
+# Pacotes NuGet
+*.nupkg
+packages /
+
+# Logs
+*.log
