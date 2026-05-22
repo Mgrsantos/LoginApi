3220b024c9e4c62d9a5ad70b0d6c96ca5b409367
mgrsantos <gabrielasaantos294@gmail.com>
2026-05-22 16:02:10 -0300

    fix: gitignore corrigido

--- LoginApi/Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LoginApi.Models
-{
-    public class User
-    {
-        public Guid Id { get; set; } = Guid.NewGuid();
-        public string Username { get; set; } = string.Empty;
-        public string PasswordHash { get; set; } = string.Empty;
-    }
-}
--- LoginApi/gitignore.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace LoginApi
-{
-    public class gitignore
-    {
-    }
-}
